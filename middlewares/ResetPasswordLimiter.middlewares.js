a8c51a1f23fe7ab7533e6945de862ceb8fb720b1
thanhqvuong <otto8758@gmail.com>
2025-07-26 15:47:56 +0700

    add api reset-password of customer and admin ratelimit

--- middlewares/ResetPasswordLimiter.middlewares.js
+++ middlewares/ResetPasswordLimiter.middlewares.js
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const resetPasswordLimiter = rateLimit({
   windowMs: 5 * 60 * 1000, // ⏳ 5 phút
-  max: 300, // ❌ tối đa 5 lần reset/password trong 5 phút
+  max: 5, // ❌ tối đa 5 lần reset/password trong 5 phút
   message: {
     message: "Too many password reset attempts. Please try again later.",
   },
