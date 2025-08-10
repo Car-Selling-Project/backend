fbc84021bb86fc78cb3e4836b977142b3a1ed1ea
thanhqvuong <otto8758@gmail.com>
2025-08-10 14:29:10 +0700

    fix function TransactionStatus

--- routes/TransactionStatus.routes.js
+++ routes/TransactionStatus.routes.js
@@ -3,13 +3,14 @@ import {
   updateTransactionStatus,
   expirePendingTransactions
 } from "../controllers/TransactionStatus.controller.js";
+import { authAdmin } from "../middlewares/AuthAdmins.middlewares.js";
 
 const TransactionStatusRouter = express.Router();
 
 // Cập nhật trạng thái transaction
-TransactionStatusRouter.patch("/:id/status", updateTransactionStatus);
+TransactionStatusRouter.patch("/transactions/:id/status",authAdmin, updateTransactionStatus);
 
 // Hủy transaction pending quá 5 phút
-TransactionStatusRouter.post("/expire", expirePendingTransactions);
+TransactionStatusRouter.post("/transactions/expire",authAdmin, expirePendingTransactions);
 
 export default TransactionStatusRouter;
