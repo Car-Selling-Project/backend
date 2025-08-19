4287af0df37fb8a457e4dbd3c1a083f7a245adfd
thanhqvuong <otto8758@gmail.com>
2025-08-20 02:54:34 +0700

    Fix function DeleteCar

--- controllers/Cars.controller.js
+++ controllers/Cars.controller.js
@@ -285,25 +285,30 @@ export const getCarById = async (req, res) => {
     });
   }
 };
-export const deleteCarByid = async(req , res) => {
-  try{
-    const {id} = req.params;
-    const deletedCar = await Car.findByIdAndUpdate(id);
-    if(!deletedCar) return res.status(404).json({
-      message: "🚫 Car not found",
-    });
+export const deleteCarById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deletedCar = await Car.findByIdAndDelete(id); 
+
+    if (!deletedCar) {
+      return res.status(404).json({
+        message: "🚫 Car not found",
+      });
+    }
+
     return res.status(200).json({
       message: "✅ Car deleted successfully",
-      data:deletedCar,
+      data: deletedCar,
     });
-  }catch(error){
+  } catch (error) {
     console.error("❌ Error deleting car:", error);
     return res.status(500).json({
       message: "🚫 Internal Server Error",
       error: error.message,
-    })
+    });
   }
-}
+};
 // Get /cars with Status active
 export const getActiveCars = async (req, res) => {
   try {
